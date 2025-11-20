5efe149e0dfffefb70b92cfedeb056ed1ecba35a
Sajal <122293590+15601012022@users.noreply.github.com>
2025-11-20 21:49:41 +0530

    Manually resolve merge conflicts and keep my latest version

--- android/app/src/main/kotlin/com/example/driver_drowsidetection/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/driver_drowsidetection/MainActivity.kt
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
+
 package com.example.driver_drowsidetection
 
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : FlutterActivity()
-=======
-package com.example.driver_drowsidetection
 
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
->>>>>>> 943ce957c49d2a336947b003b79d3109b2538ef9
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 allprojects {
     repositories {
         google()
@@ -34,40 +34,3 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
-=======
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.google.gms:google-services:4.4.4")
-    }
-}
-
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-
-subprojects {
-    project.evaluationDependsOn(":app")
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
-}
->>>>>>> 943ce957c49d2a336947b003b79d3109b2538ef9
