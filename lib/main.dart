31cf532c1a1ccb2ce72003cf67858361346bafff
Sajal <122293590+15601012022@users.noreply.github.com>
2025-11-20 20:58:47 +0530

    Update app: Add Bluetooth camera, editable settings, persistent login

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 // main.dart - Add this to set up navigation
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -56,62 +56,4 @@ class AuthGate extends StatelessWidget {
     );
   }
 }
-=======
-// main.dart - Add this to set up navigation
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'welcome_screen.dart';
-import 'signin_screen.dart';
-import 'signup_screen.dart';
-import 'account_page.dart';
-import 'main_navigation.dart';// Import your screen files here
 
-void main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp();
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Greener App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-        fontFamily: 'Roboto',
-      ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const WelcomeScreen(),
-        '/signin': (context) => const SignInScreen(),
-        '/signup': (context) => const SignUpScreen(),
-        '/account': (context) => const MainNavigation(),
-      },
-    );
-  }
-}
-
-class AuthGate extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<User?>(
-      stream: FirebaseAuth.instance.authStateChanges(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(child: CircularProgressIndicator());
-        }
-        if (snapshot.hasData && snapshot.data != null) {
-          return const MainNavigation();
-        }
-        return const WelcomeScreen();
-      },
-    );
-  }
-}
->>>>>>> 943ce957c49d2a336947b003b79d3109b2538ef9
